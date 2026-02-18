bc2f5901f3a3523a32122eca3dae5485a69ea999
nayanchoudhary31 <nayanscsit09@gmail.com>
2026-02-18 11:16:24 +0530

    feat(app): integrate cookie-parser middleware for enhanced cookie handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./routes/index.js";
+import cookieParser from "cookie-parser";
 import { globalErrorHandler } from "./middlewares/error-handler.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1/", router);
 
